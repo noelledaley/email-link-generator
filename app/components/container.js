818b12f042b788cd0345203892e7e583442033b9
Noelle Daley <adriannenoelle@gmail.com>
2020-06-10 17:41:54 -0700

    show preview of email on the right

--- app/components/container.js
+++ app/components/container.js
@@ -1,16 +1,38 @@
 import Component from "@glimmer/component";
-import { action } from "@ember/object"
+import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
-
+import { assign } from "@ember/polyfills";
 
 export default class ContainerComponent extends Component {
-  @tracked data = {};
+  @tracked email = "";
+  @tracked subject = "";
+  @tracked body = `Dear [TITLE],\n\nYour email will preview here.\n\nSincerely,\n[FULL NAME]`;
 
   @action
   updateData(evt) {
     evt.preventDefault();
     const data = $("form").serializeArray();
+    this.buildEmailPreview(data);
+  }
 
-    this.data = data;
+  @action
+  buildEmailPreview(formData) {
+    return formData.forEach((formField) => {
+      const item = formField.name;
+      return this[item] = formField.value;
+    });
+  }
+
+  @action
+  buildGmailLink(email, body, subject) {
+    return `https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=${email}&su=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+  }
+
+  buildEmailLink(email, body, subject) {
+    return `mailto:${email}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
   }
 }
--- app/components/preview.js
+++ app/components/preview.js
@@ -1,5 +1,4 @@
 import Component from "@glimmer/component";
 
 export default class PreviewComponent extends Component {
-  text = 'hello';
 }
