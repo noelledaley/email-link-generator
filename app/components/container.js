0ce4cc571d5ea35e7f7af11118973ecd18af11ec
Noelle Daley <adriannenoelle@gmail.com>
2020-06-14 09:07:53 -0700

    whitespace

--- app/components/container.js
+++ app/components/container.js
@@ -4,44 +4,44 @@ import { tracked } from "@glimmer/tracking";
 import { assign } from "@ember/polyfills";
 
 export default class ContainerComponent extends Component {
-  @tracked email = "";
-  @tracked subject = "";
-  @tracked
-  body = `Dear [TITLE],\n\nYour email will preview here.\n\nSincerely,\n[FULL NAME]`;
+                 @tracked email = "";
+                 @tracked subject = "";
+                 @tracked
+                 body = `Dear [TITLE],\n\nEnter the body of your email template here. \n\nSincerely,\n[FULL NAME]\n[CITY, STATE]`;
 
-  @tracked gmailLink = "";
-  @tracked emailLink = "";
+                 @tracked gmailLink = "";
+                 @tracked emailLink = "";
 
-  @action
-  updateData(evt) {
-    evt.preventDefault();
-    const data = $("form").serializeArray();
-    this.buildEmailPreview(data);
-  }
+                 @action
+                 updateData(evt) {
+                   evt.preventDefault();
+                   const data = $("form").serializeArray();
+                   this.buildEmailPreview(data);
+                 }
 
-  @action
-  buildEmailPreview(formData) {
-    formData.forEach((formField) => {
-      const item = formField.name;
-      return (this[item] = formField.value);
-    });
-    this.buildGmailLink();
-    this.buildEmailLink();
-  }
+                 @action
+                 buildEmailPreview(formData) {
+                   formData.forEach((formField) => {
+                     const item = formField.name;
+                     return (this[item] = formField.value);
+                   });
+                   this.buildGmailLink();
+                   this.buildEmailLink();
+                 }
 
-  @action
-  buildGmailLink() {
-    const { email, subject, body } = this;
-    return (this.gmailLink = `https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=${email}&su=${encodeURIComponent(
-      subject
-    )}&body=${encodeURIComponent(body)}`);
-  }
+                 @action
+                 buildGmailLink() {
+                   const { email, subject, body } = this;
+                   return (this.gmailLink = `https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=${email}&su=${encodeURIComponent(
+                     subject
+                   )}&body=${encodeURIComponent(body)}`);
+                 }
 
-  @action
-  buildEmailLink() {
-    const { email, subject, body } = this;
-    return (this.emailLink = `mailto:${email}?subject=${encodeURIComponent(
-      subject
-    )}&body=${encodeURIComponent(body)}`);
-  }
-}
+                 @action
+                 buildEmailLink() {
+                   const { email, subject, body } = this;
+                   return (this.emailLink = `mailto:${email}?subject=${encodeURIComponent(
+                     subject
+                   )}&body=${encodeURIComponent(body)}`);
+                 }
+               }
