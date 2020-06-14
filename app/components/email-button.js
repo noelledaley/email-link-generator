f738f7dd3525343ed98fac060879aa70ca363d3a
Noelle Daley <adriannenoelle@gmail.com>
2020-06-14 08:56:11 -0700

    add copy button

--- app/components/email-button.js
+++ app/components/email-button.js
@@ -1,4 +1,7 @@
 import Component from '@glimmer/component';
+import { tracked } from "@glimmer/tracking";
+
 
 export default class EmailButtonComponent extends Component {
+  @tracked isCopied = false;
 }
