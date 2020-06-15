b6d9011fc6f915aaca608a515ba8dac6f9715c2a
Noelle Daley <adriannenoelle@gmail.com>
2020-06-14 21:35:33 -0700

    refactor styles and rename to EmailLink

--- app/components/email-link.js
+++ app/components/email-link.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 
-export default class EmailButtonComponent extends Component {
+export default class EmailLinkComponent extends Component {
   get isCopied(){
     if (this.args.link && this.args.copiedLink) {
       return this.args.link === this.args.copiedLink;
